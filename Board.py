ea6dd2576b5997a7cb0e157df1134dcb9ec6b2d3
Junlan Zhuang <junlan.zhuang@mail.utoronto.ca>
2021-02-10 09:48:39 -0500

    UserStory2.1 complete

--- Board.py
+++ Board.py
@@ -1,11 +1,32 @@
 class Board:
 
     def __init__(self):
-        self.board = [[], [], []]
-        create_board(self.board)
+        self.empty = " "
+        self.player1 = "X"
+        self.player2 = "O"
+        self.board = self.create_board()
 
+    def create_board(self):
+        lst = [[], [], []]
+        for row in range(3):
+            for col in range(3):
+                lst.board[row][col].append(self.empty)
+        return lst
 
-def create_board(lst):
-    for row in range(3):
-        for col in range(3):
-            lst[row][col].append(" ")
+    def next_player(self):
+        return
+
+    def valid_move(self, row, col):
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            return self.board[row][col] == self.empty
+        return False
+
+    def move(self, row, col, player):
+        if self.valid_move(row, col):
+            self.board[row][col] = player
+            return True
+        return False
+
+    def check_win(self):
+        return
+
