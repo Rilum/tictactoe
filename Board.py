d2d36decb9e5107cd074ab506969f74ae13dc69d
Junlan Zhuang <junlan.zhuang@mail.utoronto.ca>
2021-02-09 21:54:06 -0500

    UserStory2 complete

--- Board.py
+++ Board.py
@@ -1,3 +1,11 @@
 class Board:
 
     def __init__(self):
+        self.board = [[], [], []]
+        create_board(self.board)
+
+
+def create_board(lst):
+    for row in range(3):
+        for col in range(3):
+            lst[row][col].append(" ")
--- Player.py
+++ Player.py
@@ -1,3 +1,5 @@
 class Player:
 
     def __init__(self):
+        self.player1 = "X"
+        self.player2 = "0"
